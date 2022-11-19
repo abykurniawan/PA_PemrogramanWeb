c92e31b3764c1236a5c41e0260f1786a6844f710
abykurniawan <abyskill@gmail.com>
2022-11-20 07:51:45 +0800

    y

--- detail.php
+++ detail.php
@@ -48,7 +48,7 @@
         <div class="profil">
             <a onclick="functiondropdown()" class="profil-toggle"><?php echo $_SESSION['nama']; ?> </a>
             <ul id="dropdown-content" class="dropdown-profil">
-                <li><a href=""><i class="fa-regular fa-user" style='padding-right: 10px;'></i>Profil</a></li>
+                <li><a href="profil.php"><i class="fa-regular fa-user" style='padding-right: 10px;'></i>Profil</a></li>
                 <li><a href="logout.php"><i class="fa-solid fa-arrow-right-from-bracket" style='padding-right: 10px;'></i>Logout</a></li>
             </ul>
         </div>
@@ -161,7 +161,6 @@
                 ?>
                     <script>
                         alert("Berhasil");
-                        window.location = "beli.php";
                     </script>
                 <?php
             }
